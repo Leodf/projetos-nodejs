65fa9fc856dfe99c288b2f932dd54fd261791757
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-06-14 16:00:33 -0300

    criando as ultimas funções

--- iniciando_com_nodejs/app.js
+++ iniciando_com_nodejs/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const { randomUUID } = require("crypto")
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.json())
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log(err);
+    } else {
+        products = JSON.parse(data);
+    }
+});
 
 /**
  * POST => Inserir um dado
@@ -33,7 +42,9 @@ app.post("/products", (request, response) => {
 
     products.push(product);
 
-    return response.json(product)
+    ProductFile();
+
+    return response.json(product);
 })
 
 app.get("/products", (request, response) => {
@@ -43,8 +54,8 @@ app.get("/products", (request, response) => {
 app.get("/products/:id", (request, response) => {
     const { id } = request.params;
     const product = products.find(product => product.id === id);
-    
-    return response.json(product)
+
+    return response.json(product);
 })
 
 app.put("/products/:id", (request, response) => {
@@ -58,6 +69,33 @@ app.put("/products/:id", (request, response) => {
        price, 
     };
 
+    ProductFile();
+
+    return response.json({ message: "Produto alterado com sucesso"});
+
 })
+
+app.delete("/products/:id", (request, response) => {
+    const { id } = request.params;
+
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products.splice(productIndex, 1);
+
+    ProductFile();
+
+    return response.json({message: "Produto removido com sucesso"}) 
+})
+
+function ProductFile () {
+    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Produto inserido");
+        }
+    });
+}
+
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
